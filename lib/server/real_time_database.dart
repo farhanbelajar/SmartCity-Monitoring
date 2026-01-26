4fdf79bd437e68bc5cb3785840d86ca7286ec47a
Muhammad Farhan <112230480+farhanbelajar@users.noreply.github.com>
2026-01-26 16:55:21 +0700

    Delete lib/server directory

--- lib/server/real_time_database.dart
+++
@@ -1,5 +0,0 @@
-import 'package:firebase_database/firebase_database.dart';
-final database = FirebaseDatabase.instance.ref();
-final Lamp1 = database.child('Lamp1');
-final Lamp2 = database.child('Lamp2');
-final LightLevel = database.child('LightLevel');
